8f4c36ce2c9e8a4184c6ef053190a9344297c3d2
mohankmn <74285276+mohankmn@users.noreply.github.com>
2020-11-28 22:06:59 +0530

    test

--- data/models.py
+++ data/models.py
@@ -5,24 +5,21 @@ from django.utils import timezone
 
 # Create your models here.
 class Items(models.Model):
-    name =models.CharField(max_length=150, db_index=True,unique=True)
-    carrying_cost=models.PositiveIntegerField(blank=True,null=True)
-    ordering_cost=models.PositiveIntegerField(blank=True,null=True)
+    name =models.CharField(max_length=150)
     
     def __str__(self):
         return "{}".format(self.name)
     
     def save(self, *args, **kwargs):
-        self.name = self.name.lower()
+        self.name = self.name.upper()
         return super().save(*args, **kwargs)
 
 class Demand(models.Model):
-    product=models.ForeignKey('Items', on_delete=models.CASCADE)
+    item=models.ForeignKey('Items', on_delete=models.CASCADE)
     price=models.PositiveIntegerField(blank=False,null=True)
     date=models.DateTimeField(default=timezone.now)
-    quantity=models.PositiveIntegerField()
-    total=models.PositiveIntegerField(blank=True,null=True)
-    Inventory_left=models.IntegerField(blank=True,null=True)
+    quantity=models.IntegerField(default='0',blank=True,null=True)
+    total=models.IntegerField(default='0',blank=True,null=True)
 
 
     def save(self,*args,**kwargs):
@@ -31,7 +28,7 @@ class Demand(models.Model):
         
     
     def __str__(self):
-        return "sold {} {} on {}".format(self.quantity,self.product.name,self.date)
+        return "sold {} {} on {}".format(self.quantity,self.item.name,self.date)
     
 
 
